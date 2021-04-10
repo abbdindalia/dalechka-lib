19ed717cd1dcc47a9ae63e164c0e5cf37c9dc031
abbdindalia <dalechka2304@mail.ru>
2021-04-10 15:01:22 +0300

    Add tests

--- tests/MyLogTest.php
+++ tests/MyLogTest.php
@@ -14,9 +14,37 @@ class MyLogTest extends TestCase {
         $this->assertInstanceOf(MyLog::class, MyLog::Instance());
     }
 
-    public function testWrite() {
-        $this->assertEquals('', MyLog::write(123));
-        $this->assertEquals('', MyLog::write("test"));
-        $this->assertEquals('', MyLog::write());
+    function testLogFileContent() {
+        $expectedString = "Test";
+        $logTime = time();
+        $logFileDir = __DIR__ . "/logs/$logTime.log";
+
+        MyLog::log($expectedString);
+        MyLog::write();
+
+        $logFileContent = file_get_contents($logFileDir);
+
+        $this->assertEquals($expectedString, $logFileContent);
+        $this->expectOutputString($expectedString);
+    }
+
+    function testCheckLogFileExist() {
+        MyLog::log(__DIR__);
+
+        $logsCounter = 0;
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter += 1;
+        }
+
+        MyLog::log($logsCounter);
+
+        MyLog::write();
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter -= 1;
+        }
+
+        $this->assertEquals(0, $logsCounter);
     }
 }
